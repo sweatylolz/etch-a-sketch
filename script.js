d126536741a4419b311dc160d5a912975457b13b
Avery Sykes <averysykes69@gmail.com>
2024-08-12 10:28:04 -0500

    divs get unique id and those divs now change color when mouseover event happens

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
 //selects the container and adds 256 divs to it 
-const container = document.getElementById("container");
+const container = document.getElementById("etchersketcher");
 
 function makeDivs(numberDivs){
     for (let i=0; i < numberDivs; i++){
         let cells = document.createElement("div");
         container.appendChild(cells);
-        cells.className = "etches";
+        cells.id = "etches" + i;
+        cells.style.backgroundColor = "white";
+        cells.style.width = "20px";
+        cells.style.height = "20px";
     }
 }
 
 makeDivs(256); 
 
+// document.getElementById("etches").addEventListener("click", clickFunction);
+
+// function clickFunction() {
+//     let cells = document.getElementById("etches");
+//     cells.style.background = "black";
+//     console.log("color changed");
+// }
+
+for (let i=0; i < 256; i++){
+document.getElementById("etches" + i).onmouseover = function() {mouseOver()};
+
+function mouseOver() {
+    document.getElementById("etches" + i).style.background = "black";
+}
+}

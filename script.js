9918f72521013f65d8623b65c8b9ac7e5bda3c60
Avery Sykes <averysykes69@gmail.com>
2024-08-12 20:45:27 -0500

    user can now be prompt to enter grid size which changes dynamically

--- script.js
+++ script.js
@@ -1,28 +1,24 @@
 //selects the container and adds 256 divs to it 
 const container = document.getElementById("etchersketcher");
 
-function makeDivs(numberDivs){
-    for (let i=0; i < numberDivs; i++){
+let etchSize = Number(prompt("Type the size you would like grid to be (1-99)."));
+let etchGrid = etchSize * etchSize;
+let etchPixelSize = 360 / etchSize; //360 is the height and width of the container
+
+function makeDivs(etchGrid){
+    for (let i=0; i < etchGrid; i++){
         let cells = document.createElement("div");
         container.appendChild(cells);
         cells.id = "etches" + i;
         cells.style.backgroundColor = "white";
-        cells.style.width = "20px";
-        cells.style.height = "20px";
+        cells.style.width = etchPixelSize + "px";
+        cells.style.height = etchPixelSize + "px";
     }
 }
 
-makeDivs(256); 
+makeDivs(etchGrid); 
 
-// document.getElementById("etches").addEventListener("click", clickFunction);
-
-// function clickFunction() {
-//     let cells = document.getElementById("etches");
-//     cells.style.background = "black";
-//     console.log("color changed");
-// }
-
-for (let i=0; i < 256; i++){
+for (let i=0; i < etchGrid; i++){
 document.getElementById("etches" + i).onmouseover = function() {mouseOver()};
 
 function mouseOver() {

21fbabc3fde7d15b475b40b8dc64a1fd17174b4c
Avery Sykes <averysykes69@gmail.com>
2024-08-19 13:34:35 -0500

    user can now click and etch

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 //selects the container and adds 256 divs to it 
 const container = document.getElementById("etchersketcher");
 
+
 let etchSize = Number(prompt("Type the size you would like grid to be (1-99)."));
 let etchGrid = etchSize * etchSize;
 let etchPixelSize = 360 / etchSize; //360 is the height and width of the container
@@ -8,8 +9,8 @@ let etchPixelSize = 360 / etchSize; //360 is the height and width of the contain
 function makeDivs(etchGrid){
     for (let i=0; i < etchGrid; i++){
         let cells = document.createElement("div");
+        cells.classList = "etches";
         container.appendChild(cells);
-        cells.id = "etches" + i;
         cells.style.backgroundColor = "white";
         cells.style.width = etchPixelSize + "px";
         cells.style.height = etchPixelSize + "px";
@@ -18,10 +19,12 @@ function makeDivs(etchGrid){
 
 makeDivs(etchGrid); 
 
-for (let i=0; i < etchGrid; i++){
-document.getElementById("etches" + i).onmouseover = function() {mouseOver()};
+const gridCells = document.querySelectorAll(".etches");
 
-function mouseOver() {
-    document.getElementById("etches" + i).style.background = "black";
-}
-}
+gridCells.forEach(cell => { 
+    cell.addEventListener("mouseover", evt => {
+      if (evt.buttons === 1) { //if user holds click they can 'draw' 
+        cell.style.backgroundColor = "black";
+      }
+    });
+  });
